a692db8d094ae301a5d5485677d52cf25f6e1d6d
F4GH3R <95624570+F4GH3R@users.noreply.github.com>
2022-03-21 17:11:22 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,39 @@
+//plan storlek
+var sizePx = 10;
+const generated = [];
+shuffler = () => {
+    let assign = sizePx * sizePx;
+    for(let x = 1; x <= sizePx; x++){
+    let r = Math.floor(Math.random() * assign) + 1;
+    generated.push(r);
+    }
+    return generated;
+}
 
+function tableCreator(){
+    var tile = 0;
+    for(let x = 1; x <= sizePx; x++){        
+        var row = document.createElement('tr')
+        row.setAttribute('id', 'row ' + x)
+        tableVar = document.getElementById('tableid');
+        tableVar.appendChild(row);
+        for(let y = 1; y <= sizePx; y++){
+            tile = tile + 1;
+            var column = document.createElement('td');
+            column.setAttribute('id', tile);
+            row.appendChild(column);
+            let hidden = '';
+            if (generated.includes(tile)){
+               hidden = 'bomb';
+            } else {
+                hidden = 'facingDown'
+            }
+            var hiddenDiv = document.createElement('img');
+            hiddenDiv.src = 'images/' + hidden + '.png';
+            column.appendChild(hiddenDiv);
+        }
+    }
+    console.log(tableVar)
+}
+shuffler();
+tableCreator()
