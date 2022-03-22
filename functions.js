e0c7cf5958b56e353c78a3bb7e5e3c78b403db0e
F4GH3R <95624570+F4GH3R@users.noreply.github.com>
2022-03-22 10:07:46 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,16 +1,19 @@
 //plan storlek
-var sizePx = 10;
+var sizePx = 20;
 const generated = [];
 shuffler = () => {
     let assign = sizePx * sizePx;
-    for(let x = 1; x <= sizePx; x++){
+    let bombcount = ((sizePx/10)*sizePx*1.25);
+    for(let x = 1; x <= bombcount; x++){
     let r = Math.floor(Math.random() * assign) + 1;
     generated.push(r);
     }
     return generated;
 }
+shuffler();
 
 function tableCreator(){
+    //var imageDiv = 0;
     var tile = 0;
     for(let x = 1; x <= sizePx; x++){        
         var row = document.createElement('tr')
@@ -23,17 +26,69 @@ function tableCreator(){
             column.setAttribute('id', tile);
             row.appendChild(column);
             let hidden = '';
+        //tull
             if (generated.includes(tile)){
                hidden = 'bomb';
             } else {
-                hidden = 'facingDown'
+                hidden = '0'
             }
-            var hiddenDiv = document.createElement('img');
-            hiddenDiv.src = 'images/' + hidden + '.png';
-            column.appendChild(hiddenDiv);
+            var imageDiv= document.createElement('img');
+            imageDiv.setAttribute('id', 'imgid:' + tile)
+            imageDiv.src = 'images/' + hidden + '.png';
+            column.appendChild(imageDiv);
+            column.setAttribute('onmousedown', 'downfunc(this.id)')
+            column.setAttribute('onmouseup', 'upfunc(this.id)')
         }
     }
-    console.log(tableVar)
+    return imageDiv;
 }
-shuffler();
-tableCreator()
+tableCreator();
+
+function downfunc(downVar){  
+    document.getElementById(downVar).childNodes[0].src = 'images/0.png';   
+}
+function upfunc(upVar){
+
+    if(generated.includes(Number(upVar))){
+        console.log('Du klickade på en bomb!')
+        document.getElementById(upVar).childNodes[0].src = 'images/boom.png';
+    } else {
+        console.log('Du klickade inte på en bomb!')
+        document.getElementById(upVar).childNodes[0].src = 'images/1.png';
+    }
+
+    //ekolod
+    var ekoNumber = 0;
+    for(let g = 0; g < 3; g++){
+        let ekoVar = 0;
+        for(let h = -1; h < 2; h++){
+            //
+            if(g === 0){
+                ekoVar = document.getElementById(Number(upVar) - sizePx + h);
+                if(ekoVar.childNodes[0].src.includes('images/bomb.png')){
+                    ekoNumber++;
+                } else if (g === 1){
+                ekoVar = document.getElementById(Number(upVar) + h);
+                if(ekoVar.childNodes[0].src.includes('images/bomb.png')){
+                    ekoNumber++;
+                }
+            } else{
+                ekoVar = document.getElementById(Number(upVar) + sizePx + h);
+                if(ekoVar.childNodes[0].src.includes('images/bomb.png')){
+                    ekoNumber++;
+                }
+            }
+            
+           //console.log(upVar);
+           
+            }
+        }
+        
+    }
+    console.log(ekoNumber);
+}
+//Kasta in Ekolod i tablecreator 
+// fixa bombräkning
+
+    
+
